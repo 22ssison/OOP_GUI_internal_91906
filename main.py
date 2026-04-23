108d761cf0390e7be340305bc1164108d641cbf2
22ssison <22ssison@wakatipu.school.nz>
2026-04-23 13:30:27 +1200

    started to code

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-# test
-# another test
+"""
+NCEA Level 3 Chemistry Quiz: Properties of Organic Compounds
+------------------------------------------------------------
+An interactive GUI application developed using Tkinter and OOP principles.
+This program evaluates knowledge of organic functional groups and reactions 
+through a dynamic multiple-choice interface.
+"""
+
+
+class Question:
+    def __init__(self, txt, options, ans_index, marks):
+        self.text = text # question
+        self.options = options # different options in list
+        self.ans_index = ans_index # index of correct answer
+        self.marks = marks # how many marks the question is worth
+
+class OrgQuiz:
+    def __init__(self, parent):
+        self.questions = [ 
+            Question("")]
